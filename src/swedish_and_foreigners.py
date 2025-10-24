b04367311ce774711d08e80739151fea626279ca
AlyssaU3 <alys4321c@gmail.com>
2025-10-24 10:08:10 -0400

    56 done

--- src/swedish_and_foreigners.py
+++ src/swedish_and_foreigners.py
@@ -3,11 +3,17 @@
 import pandas as pd
 
 def swedish_and_foreigners():
-    pass
-
+    read=pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    municipalities= read["Akaa": "Äänekoski"]
+    mask_speaking= municipalities["Share of Swedish-speakers of the population, %"]>5
+    foreigners_mask= municipalities["Share of foreign citizens of the population, %"]>5
+    masks= municipalities[mask_speaking & foreigners_mask]
+    only_needed=masks[["Population","Share of Swedish-speakers of the population, %","Share of foreign citizens of the population, %"]]
+    return only_needed
+    # print(only_needed)
 
 def main():
-    swedish_and_foreigners()
+    print(swedish_and_foreigners())
 
 if __name__ == "__main__":
     main()
